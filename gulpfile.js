2bf56b5f701455a6f71978067f8cab04f2fa0aab
waldemirflj <waldemirflj@gmail.com>
2019-04-23 20:22:14 -0300

    Adiciona taks de build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 const del = require('del')
 const gulp = require('gulp')
-
-const libs = [
-  './node_modules/angular/angular.min.js',
-  './node_modules/oclazyload/dist/ocLazyLoad.min.js',
-  './node_modules/angular-sanitize/angular-sanitize.min.js',
-  './node_modules/@uirouter/angularjs/release/angular-ui-router.min.js',
-]
+const htmlmin = require('gulp-htmlmin')
+const uglify = require('gulp-uglify-es').default
+const concat = require('gulp-concat')
+const gulpif = require('gulp-if')
+const htmlreplace = require('gulp-html-replace')
 
 function clean() {
   return del([
@@ -14,9 +12,69 @@ function clean() {
   ])
 }
 
+function cleanWww() {
+  return del([
+    './www',
+  ])
+}
+
 gulp.task('libs', () => {
-  return gulp.src(libs)
+  const files = [
+    './node_modules/angular/angular.min.js',
+    './node_modules/oclazyload/dist/ocLazyLoad.min.js',
+    './node_modules/angular-sanitize/angular-sanitize.min.js',
+    './node_modules/@uirouter/angularjs/release/angular-ui-router.min.js',
+  ]
+
+  return gulp.src(files)
     .pipe(gulp.dest('./src/assets/libs/'))
 })
+
+gulp.task('www', () => {
+  return gulp.src('./src/assets/**/*')
+    .pipe(gulp.dest('./www/assets'))
+})
+
+gulp.task('replace', async () => {
+  gulp.src('./src/index.html')
+    .pipe(htmlreplace({ 'app': './bundle.js' }))
+    .pipe(gulp.dest('./www'))
+})
+
+gulp.task('minify:html', async () => {
+  gulp.src('./src/views/**/*.html')
+    .pipe(htmlmin({ removeComments: true, collapseWhitespace: true }))
+    .pipe(gulp.dest('./www/views'))
+})
+
+gulp.task('minify:js:app', async () => {
+  const files = [
+    './src/app.module.js',
+    './src/app.constants.js',
+    './src/app.config.js',
+  ];
+
+  gulp.src(files)
+    .pipe(uglify())
+    .on('error', (error) => console.log(error.toString()))
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('./www/'))
+})
+
+gulp.task('minify:js:controller', async () => {
+  gulp.src('./src/views/**/*.js')
+    .pipe(uglify())
+    .on('error', (error) => console.log(error.toString()))
+    .pipe(gulp.dest('./www/views'))
+})
+
+exports.build = gulp.series(cleanWww, gulp.series([
+  'libs',
+  'www',
+  'replace',
+  'minify:html',
+  'minify:js:app',
+  'minify:js:controller',
+]))
 
 exports.default = gulp.series(clean, gulp.series(['libs']))
